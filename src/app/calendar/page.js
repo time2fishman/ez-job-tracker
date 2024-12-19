ea9a898ccfcff3d16ae3eb53ed2060cdb0ced832
time2fishman <31023666+time2fishman@users.noreply.github.com>
2024-12-19 11:36:01 -0700

    build calendar page

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,7 +1,17 @@
-import React from 'react'
-
 export default function Calendar() {
+  const FULL_CALENDAR = process.env.QUICK_VIEW_CALENDAR
+
   return (
-    <h1 className="text-white text-4xl text-center">Calendar Page</h1>
+    <main>
+      <h1 className="text-white text-4xl text-center mt-10 mb-10">Calendar</h1>
+      <div className='responsiveCal'>
+        <div className='deskContent'>
+          <iframe src={FULL_CALENDAR} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+        </div>
+        <div className='phoneContent'>
+          <iframe src={FULL_CALENDAR} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+        </div>
+      </div>
+    </main>
   )
 }
