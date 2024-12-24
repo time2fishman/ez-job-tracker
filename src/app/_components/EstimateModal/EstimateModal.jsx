7b4a0376c8044917b6f2a796c7d9731c5c45b4a8
time2fishman <31023666+time2fishman@users.noreply.github.com>
2024-12-24 10:15:37 -0700

    EstimateModal template hard coded

--- src/app/_components/EstimateModal/EstimateModal.jsx
+++ src/app/_components/EstimateModal/EstimateModal.jsx
@@ -5,22 +5,22 @@ function EstimateModal({ isOpen, onClose }) {
 
   return (
     <div className="modal-container text-black">
-      <div className="mt-4 flex">
+      <div className="flex">
         <img
-          alt="Your Company"
+          alt="Demo Company"
           src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
           className="h-16 w-auto"
         />
-        <h1 className="text-4xl grow text-right content-center">ESTIMATE</h1>
+        <h1 className="text-3xl grow text-right content-center">ESTIMATE</h1>
       </div>
       <div>
-        <h2 className="text-2xl text-gray-600 mt-4 mb-2">Demo Company</h2>
+        <h2 className="text-2xl text-gray-700 mt-4 mb-2">Demo Company</h2>
         <p className="text-gray-500 py-1">1234 Fake Street</p>
         <p className="text-gray-500 mb-10">Somewhere, MI 12345</p>
       </div>
       <div className="flex">
         <div>
-          <h3 className="text-lg mb-2">Bill To:</h3>
+          <h3>Bill To:</h3>
           <p className="text-gray-500 py-1">John Doe</p>
           <p className="text-gray-500 py-1">35 Wicked Rd</p>
           <p className="text-gray-500 pt-1">Rancho, MI 12344</p>
@@ -39,9 +39,9 @@ function EstimateModal({ isOpen, onClose }) {
         </div>
       </div>
       <div className="flex justify-center">
-        <table className="w-11/12 mb-10 border border-black">
+        <table className="w-full mb-10 border border-gray-400">
           <thead>
-            <tr className="bg-gray-600 text-gray-300 text-left border border-black">
+            <tr className="bg-gray-600 text-gray-300 text-left border border-gray-400">
               <th className="p-2">Item Description</th>
               <th className="p-2">Qty</th>
               <th className="p-2">Rate</th>
@@ -49,37 +49,37 @@ function EstimateModal({ isOpen, onClose }) {
             </tr>
           </thead>
           <tbody>
-            <tr className="border border-black">
+            <tr className="border border-gray-400">
               <td className="p-2">Labor</td>
-              <td className="p-2">7</td>
-              <td className="p-2">50.00</td>
-              <td className="p-2">350</td>
+              <td className="p-2 border-l border-gray-400">7</td>
+              <td className="p-2 border-l border-gray-400">50.00</td>
+              <td className="p-2 border-l border-gray-400">350.00</td>
             </tr>
-            <tr className="border border-black">
+            <tr className="border border-gray-400">
               <td className="p-2">Materials</td>
-              <td className="p-2">N/A</td>
-              <td className="p-2">N/A</td>
-              <td className="p-2">650</td>
+              <td className="p-2 border-l border-gray-400">N/A</td>
+              <td className="p-2 border-l border-gray-400">N/A</td>
+              <td className="p-2 border-l border-gray-400">650.00</td>
             </tr>
           </tbody>
           <tfoot>
-            <tr>
+            {/* <tr>
               <td className="pt-6"></td>
               <td className="pt-6"></td>
               <td className="pt-6">Sub Total</td>
               <td className="pt-6">1000.00</td>
-            </tr>
-            <tr>
+            </tr> */}
+            {/* <tr>
               <td className="pt-1"></td>
               <td className="pt-1"></td>
               <td className="pt-1">Sales Tax</td>
               <td className="pt-1">100.00</td>
-            </tr>
+            </tr> */}
             <tr>
-              <td className="pt-4"></td>
-              <td className="pt-4"></td>
-              <td className="pt-4 bg-gray-400 text-lg">Total</td>
-              <td className="pt-4 bg-gray-400">$ 1100.00</td>
+              <td className="p-2"></td>
+              <td className="p-2"></td>
+              <td className="text-lg pt-6 pb-2 px-2">Total</td>
+              <td className="pt-6 pb-2 px-2">1100.00</td>
             </tr>
           </tfoot>
         </table>
