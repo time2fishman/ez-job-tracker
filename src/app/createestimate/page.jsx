28720e33077a93b08db99871a887997a62c38205
time2fishman <31023666+time2fishman@users.noreply.github.com>
2025-01-02 19:18:28 -0700

    styled create estimate page

--- src/app/createestimate/page.jsx
+++ src/app/createestimate/page.jsx
@@ -1,68 +1,260 @@
+"use client";
+
+import { useState } from "react";
+
 export default function CreateEstimatePage() {
+  const [rows, setRows] = useState([]);
+
+  const addRow = () => {
+    setRows([
+      ...rows,
+      {
+        id: Date.now(),
+        itemDescription: "",
+        qty: "",
+        rate: "",
+        amount: "",
+      },
+    ]);
+  };
+
+  const handleInputChange = (id, field, value) => {
+    setRows((prevRows) =>
+      prevRows.map((row) => (row.id === id ? { ...row, [field]: value } : row))
+    );
+  };
+
   return (
-    <div className="bg-white h-screen w-screen text-black">
-      <h1 className="text-center text-4xl pt-10 pb-10">Create New Estimate</h1>
-      <form action={""} method="post">
-        <fieldset className="border">
-          <legend>Customer information:</legend>
-          <label htmlFor="fname">First Name: </label>
-          <input type="text" id="fname" name="fname" className="border" />
-          <br />
-          <label htmlFor="lname">Last Name: </label>
-          <input type="text" id="lname" name="lname" className="border" />
-          <br />
-          <label htmlFor="email">Email: </label>
-          <input type="email" id="email" name="email" className="border" />
-          <br />
-          <label htmlFor="address">Address: </label>
-          <input type="text" id="address" name="address" className="border" />
-          <br />
-          <label htmlFor="city">City: </label>
-          <input type="text" id="city" name="city" className="border" />
-          <br />
-          <label htmlFor="state">State: </label>
-          <input type="text" id="state" name="state" className="border" />
-          <br />
-          <label htmlFor="zip">Zip: </label>
-          <input type="text" id="zip" name="zip" className="border" />
+    <div className="m-auto bg-gray-100 text-black min-w-fit max-w-4xl">
+      <h1 className="text-4xl pt-10 pb-10 ml-4">Create New Estimate</h1>
+      <form action={""} id="form" method="post" className="mx-8">
+        <fieldset className="mb-8">
+          <legend className="text-2xl mb-8">Customer Information</legend>
+          <div className="max-w-xs px-4">
+            <div>
+              <label htmlFor="fname" className="block">
+                First Name
+              </label>
+              <input
+                maxLength={50}
+                type="text"
+                id="fname"
+                name="fname"
+                className="border px-1 mb-2 w-full"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="lname" className="block">
+                Last Name
+              </label>
+              <input
+                maxLength={25}
+                type="text"
+                id="lname"
+                name="lname"
+                className="border px-1 mb-2 w-full"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block">
+                Email
+              </label>
+              <input
+                maxLength={25}
+                type="email"
+                id="email"
+                name="email"
+                className="border px-1 mb-2 w-full"
+              />
+            </div>
+            <div>
+              <label htmlFor="address" className="block">
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                className="border px-1 mb-2 w-full"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="city" className="block">
+                City
+              </label>
+              <input
+                type="text"
+                id="city"
+                name="city"
+                className="border px-1 mb-2 w-full"
+                maxLength={25}
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="state" className="block">
+                State
+              </label>
+              <input
+                type="text"
+                id="state"
+                name="state"
+                className="border px-1 mb-2"
+                minLength={2}
+                maxLength={2}
+                size={2}
+                height={26}
+                required
+              />
+            </div>
+            <div>
+              <label className="block">Zip</label>
+              <input
+                type="text"
+                id="zip"
+                name="zip"
+                className="px-1 border mb-2"
+                minLength={5}
+                maxLength={5}
+                size={5}
+                required
+              />
+            </div>
+            <div>
+              <label className="block">Phone</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                className="px-1 border mb-10 placeholder:text-center"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                maxLength={12}
+                size={13}
+                placeholder="000-000-0000"
+                required
+              />
+            </div>
+          </div>
         </fieldset>
-        <fieldset className="border">
-          <legend>Job Details:</legend>
-          <table className="mb-10 border border-gray-400">
-            <thead>
-              <tr className="bg-gray-600 text-gray-300 text-left border border-gray-400">
-                <th className="p-2">Item Description</th>
-                <th className="p-2">Qty</th>
-                <th className="p-2">Rate</th>
-                <th className="p-2">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border border-gray-400">
-                <td className="p-2">
-                  <textarea
-                    type="text"
-                    id="item-description"
-                    name="item-description"
-                    className="border"
-                  />
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  <input type="text" id="qty" name="qty" className="border" />
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  <input type="text" id="rate" name="rate" className="border" />
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  <input
-                    type="text"
-                    id="amount"
-                    name="amount"
-                    className="border"
-                  />
-                </td>
-              </tr>
-              {/* <tr className="border border-gray-400">
+        <fieldset>
+          <legend className="text-2xl mb-8">Job Details</legend>
+          <div className="px-4">
+            <table className="w-full mb-10 border border-gray-400">
+              <thead>
+                <tr className="bg-gray-600 text-gray-300 text-left border border-gray-400">
+                  <th className="p-2">Item Description</th>
+                  <th className="p-2">Qty</th>
+                  <th className="p-2">Rate</th>
+                  <th className="p-2">Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="border border-gray-400">
+                  <td className="p-2">
+                    <textarea
+                      type="text"
+                      id="item-description"
+                      name="item-description"
+                      className="border flex justify-self-center py-1 px-2 leading-snug"
+                      rows={3}
+                      maxLength={75}
+                      required
+                    />
+                  </td>
+                  <td className="p-2 border-l border-gray-400 text-right">
+                    <input
+                      type="text"
+                      id="qty"
+                      name="qty"
+                      className="border flex justify-self-center text-right pr-2"
+                      required
+                    />
+                  </td>
+                  <td className="p-2 border-l border-gray-400 text-right">
+                    <input
+                      type="text"
+                      id="rate"
+                      name="rate"
+                      className="border flex justify-self-center text-right pr-2"
+                      required
+                    />
+                  </td>
+                  <td className="p-2 border-l border-gray-400 text-right">
+                    <input
+                      type="text"
+                      id="amount"
+                      name="amount"
+                      className="border flex justify-self-center text-right pr-2"
+                      required
+                    />
+                  </td>
+                </tr>
+                {rows.map((row) => (
+                  <tr key={row.id} className="border border-gray-400">
+                    <td className="p-2">
+                      <textarea
+                        type="text"
+                        value={row.firstName}
+                        onChange={(e) =>
+                          handleInputChange(row.id, "firstName", e.target.value)
+                        }
+                        className="border flex justify-self-center py-1 px-2 leading-snug"
+                        rows={3}
+                        maxLength={75}
+                        required
+                      />
+                    </td>
+                    <td className="p-2 border-l border-gray-400 text-right">
+                      <input
+                        type="text"
+                        value={row.qty}
+                        onChange={(e) =>
+                          handleInputChange(row.id, "qty", e.target.value)
+                        }
+                        className="border flex justify-self-center text-right pr-2"
+                        required
+                      />
+                    </td>
+                    <td className="p-2 border-l border-gray-400 text-right">
+                      <input
+                        type="text"
+                        value={row.rate}
+                        onChange={(e) =>
+                          handleInputChange(row.id, "rate", e.target.value)
+                        }
+                        className="border flex justify-self-center text-right pr-2"
+                        required
+                      />
+                    </td>
+                    <td className="p-2 border-l border-gray-400 text-right ">
+                      <input
+                        type="text"
+                        value={row.amount}
+                        onChange={(e) =>
+                          handleInputChange(row.id, "amount", e.target.value)
+                        }
+                        className="border flex justify-self-center text-right pr-2"
+                        required
+                      />
+                    </td>
+                  </tr>
+                ))}
+                <tr>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={addRow}
+                      className="border-2 border-gray-500 rounded py-1 px-2 mt-4 ml-2 text-gray-500 mb-4"
+                    >
+                      + Add Row
+                    </button>
+                  </td>
+                </tr>
+                {/* <tr className="border border-gray-400">
                 <td className="p-2">{estimateData[0].itemDescription2}</td>
                 <td className="p-2 border-l border-gray-400 text-right">
                   {estimateData[0].qty2}
@@ -74,23 +266,32 @@ export default function CreateEstimatePage() {
                   {estimateData[0].amount1}
                 </td>
               </tr> */}
-            </tbody>
-            <tfoot>
-              <tr>
-                <td className="p-2"></td>
-                <td className="p-2"></td>
-                <td className="text-lg pt-6 pb-2 px-2">Total</td>
-                <td className="pt-6 pb-2 px-2 text-right">
-                  <input
-                    type="text"
-                    id="total"
-                    name="total"
-                    className="border"
-                  />
-                </td>
-              </tr>
-            </tfoot>
-          </table>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td className="p-2"></td>
+                  <td className="p-2"></td>
+                  <td className="text-lg text-right pt-6 pb-4 px-2">Total</td>
+                  <td className="pt-6 px-2 pb-4 text-right">
+                    <input
+                      type="text"
+                      id="total"
+                      name="total"
+                      className="border flex justify-self-center text-right pr-2"
+                    />
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="border bg-indigo-700 rounded py-2 px-4 text-gray-300 mb-10"
+              >
+                Create Estimate
+              </button>
+            </div>
+          </div>
         </fieldset>
       </form>
     </div>
