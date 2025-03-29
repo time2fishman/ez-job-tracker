5af3963fc26e938c8730a491cfa26d1311ddffbd
time2fishman <31023666+time2fishman@users.noreply.github.com>
2025-03-28 19:04:05 -0600

    fix open calendar button on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Home() {
-  const quickViewCalendar = process.env.QUICK_VIEW_CALENDAR
+  const quickViewCalendar = process.env.QUICK_VIEW_CALENDAR;
 
   return (
     <>
@@ -10,22 +10,36 @@ export default function Home() {
       </h1>
       <main>
         <div className=" flex justify-center mt-8 mb-16">
-          <Link href={'/pages/createestimate'}>
-            <button className="mr-2 bg-indigo-700 p-3.5 border rounded font-semibold hover:bg-indigo-500 active:bg-indigo-800">Create New Estimate</button>
+          <Link href={"/pages/createestimate"}>
+            <button className="mr-2 bg-indigo-700 p-3.5 border rounded font-semibold hover:bg-indigo-500 active:bg-indigo-800">
+              Create New Estimate
+            </button>
           </Link>
-          <Link href={'/invoices'}>
-            <button className="ml-2 bg-indigo-700 p-3.5 border rounded font-semibold hover:bg-indigo-500 active:bg-indigo-800 ">Create New Invoice</button>
+          <Link href={"/invoices"}>
+            <button className="ml-2 bg-indigo-700 p-3.5 border rounded font-semibold hover:bg-indigo-500 active:bg-indigo-800 ">
+              Create New Invoice
+            </button>
           </Link>
         </div>
         <div className="calendar-container">
           <div className="flex calendar-text">
             <h2 className="text-center text-2xl mb-2">Calendar Quick View</h2>
-            <a className="flex justify-self-center mb-5 text-gray-400 hover:bg-gray-800 hover:text-gray-200 active:text-white p-1.5 hover:rounded hover:border Íactive:rounded active:bg-gray-600" href="/calendar" >Open Calendar</a>
+            <Link href={"/pages/calendar"}>
+              <button className="flex justify-self-center mb-5 text-gray-400 hover:bg-gray-800 hover:text-gray-200 active:text-white p-1.5 hover:rounded hover:border Íactive:rounded active:bg-gray-600">
+                Open Calendar
+              </button>
+            </Link>
           </div>
-          <iframe className="quick-view-calendar flex justify-self-center mb-20" src={quickViewCalendar} width={0} height={0} frameBorder="0" scrolling="no"></iframe>
-
+          <iframe
+            className="quick-view-calendar flex justify-self-center mb-20"
+            src={quickViewCalendar}
+            width={0}
+            height={0}
+            frameBorder="0"
+            scrolling="no"
+          ></iframe>
         </div>
-      </main >
+      </main>
     </>
   );
 }
