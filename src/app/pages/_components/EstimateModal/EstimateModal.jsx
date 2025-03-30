aa03aaffcf785b3f7f93cac19a732e88e41fb27d
time2fishman <31023666+time2fishman@users.noreply.github.com>
2025-03-30 12:34:58 -0600

    fix logo in EstimateModal and worked on download pdf button

--- src/app/pages/_components/EstimateModal/EstimateModal.jsx
+++ src/app/pages/_components/EstimateModal/EstimateModal.jsx
@@ -1,152 +1,153 @@
 import "./EstimateModal.css";
-// import jsPDF from "jspdf";
+import jsPDF from "jspdf";
 import data from "@/app/data/client-data.json";
 import estimateData from "@/app/data/estimates.json";
-// import html2canvas from "html2canvas";
-// import React, { useRef } from "react";
+import Logo from "../../../../../public/Adam_logo.png";
+import Image from "next/image";
+import html2canvas from "html2canvas";
+import React, { useRef } from "react";
 
 function EstimateModal({ isOpen, onClose }) {
   if (!isOpen) return null;
 
-  // const pdfRef = useRef();
+  const pdfRef = useRef();
 
-  // const downloadPDF = () => {
-  //   const input = pdfRef.current;
-  //   html2canvas(input).then((canvas) => {
-  //     const imgData = canvas.toDataURL("image/png");
-  //     const pdf = new jsPDF("p", "mm", "a4", true);
-  //     const pdfWidth = pdf.internal.pageSize.getWidth();
-  //     const pdfHeight = pdf.internal.pageSize.getHeight();
-  //     const imgWidth = canvas.width;
-  //     const imgHeight = canvas.height;
-  //     const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
-  //     const imgX = (pdfWidth - imgWidth * ratio) / 2;
-  //     const imgY = 30;
-  //     pdf.addImage(
-  //       imgData,
-  //       "PNG",
-  //       imgX,
-  //       imgY,
-  //       imgWidth * ratio,
-  //       imgHeight * ratio
-  //     );
-  //     pdf.save("estimate.pdf");
-  //   });
-  // };
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = 10;
+      pdf.addImage(
+        imgData,
+        "PNG",
+        imgX,
+        imgY,
+        imgWidth * ratio,
+        imgHeight * ratio
+      );
+      pdf.save("estimate.pdf");
+    });
+  };
 
   return (
-    <>
-      <div className="modal-container text-black">
-        <div className="flex">
-          <img
-            alt="Demo Company"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-            className="h-16 w-auto"
-          />
-          <h1 className="text-3xl grow text-right content-center">ESTIMATE</h1>
-        </div>
+    <div ref={pdfRef} className="modal-container text-black">
+      <div className="grid grid-cols-3 mx-8 mt-8 mb-4">
+        <Image
+          alt="Demo Company"
+          src={Logo}
+          className="h-24 w-auto col-start-1"
+        />
+        <h1 className="text-3xl col-start-3 content-center text-right">
+          ESTIMATE
+        </h1>
+      </div>
+      <div>
+        <h2 className="text-2xl text-gray-700 mt-4 mb-2">Demo Company</h2>
+        <p className="text-gray-500 py-1">1234 Fake Street</p>
+        <p className="text-gray-500 mb-10">Somewhere, MI 12345</p>
+      </div>
+      <div className="flex">
         <div>
-          <h2 className="text-2xl text-gray-700 mt-4 mb-2">Demo Company</h2>
-          <p className="text-gray-500 py-1">1234 Fake Street</p>
-          <p className="text-gray-500 mb-10">Somewhere, MI 12345</p>
+          <h3>Bill To:</h3>
+          <p className="text-gray-500 py-1">
+            {data[0].firstName} {data[0].lastName}
+          </p>
+          <p className="text-gray-500 py-1">{data[0].address}</p>
+          <p className="text-gray-500 pt-1">
+            {data[0].city}, {data[0].state} {data[0].zip}
+          </p>
         </div>
-        <div className="flex">
+        <div className="flex grow justify-center mb-20">
           <div>
-            <h3>Bill To:</h3>
-            <p className="text-gray-500 py-1">
-              {data[0].firstName} {data[0].lastName}
-            </p>
-            <p className="text-gray-500 py-1">{data[0].address}</p>
-            <p className="text-gray-500 pt-1">
-              {data[0].city}, {data[0].state} {data[0].zip}
-            </p>
+            <p className="py-1">Estimate#</p>
+            <p className="py-1">Estimate Date </p>
+            <p className="pt-1">Due Date</p>
           </div>
-          <div className="flex grow justify-center mb-20">
-            <div>
-              <p className="py-1">Estimate#</p>
-              <p className="py-1">Estimate Date </p>
-              <p className="pt-1">Due Date</p>
-            </div>
-            <div>
-              <p className="text-gray-500 pl-5 py-1">
-                {estimateData[0].estimateId}
-              </p>
-              <p className="text-gray-500 pl-5 py-1">
-                {estimateData[0].estimateDate}
-              </p>
-              <p className="text-gray-500 pl-5 pt-1">
-                {estimateData[0].dueDate}
-              </p>
-            </div>
+          <div>
+            <p className="text-gray-500 pl-5 py-1">
+              {estimateData[0].estimateId}
+            </p>
+            <p className="text-gray-500 pl-5 py-1">
+              {estimateData[0].estimateDate}
+            </p>
+            <p className="text-gray-500 pl-5 pt-1">{estimateData[0].dueDate}</p>
           </div>
         </div>
-        <div className="flex justify-center">
-          <table className="w-full mb-10 border border-gray-400">
-            <thead>
-              <tr className="bg-gray-600 text-gray-300 text-left border border-gray-400">
-                <th className="p-2">Item Description</th>
-                <th className="p-2">Qty</th>
-                <th className="p-2">Rate</th>
-                <th className="p-2">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border border-gray-400">
-                <td className="p-2">{estimateData[0].itemDescription1}</td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  {estimateData[0].qty1}
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  {estimateData[0].rate1}
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  {estimateData[0].amount1}
-                </td>
-              </tr>
-              <tr className="border border-gray-400">
-                <td className="p-2">{estimateData[0].itemDescription2}</td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  {estimateData[0].qty2}
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  {estimateData[0].rate2}
-                </td>
-                <td className="p-2 border-l border-gray-400 text-right">
-                  {estimateData[0].amount1}
-                </td>
-              </tr>
-            </tbody>
-            <tfoot>
-              {/* <tr>
+      </div>
+      <div className="flex justify-center">
+        <table className="w-full mb-10 border border-gray-400">
+          <thead>
+            <tr className="bg-gray-600 text-gray-300 text-left border border-gray-400">
+              <th className="p-2">Item Description</th>
+              <th className="p-2">Qty</th>
+              <th className="p-2">Rate</th>
+              <th className="p-2">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border border-gray-400">
+              <td className="p-2">{estimateData[0].itemDescription1}</td>
+              <td className="p-2 border-l border-gray-400 text-right">
+                {estimateData[0].qty1}
+              </td>
+              <td className="p-2 border-l border-gray-400 text-right">
+                {estimateData[0].rate1}
+              </td>
+              <td className="p-2 border-l border-gray-400 text-right">
+                {estimateData[0].amount1}
+              </td>
+            </tr>
+            <tr className="border border-gray-400">
+              <td className="p-2">{estimateData[0].itemDescription2}</td>
+              <td className="p-2 border-l border-gray-400 text-right">
+                {estimateData[0].qty2}
+              </td>
+              <td className="p-2 border-l border-gray-400 text-right">
+                {estimateData[0].rate2}
+              </td>
+              <td className="p-2 border-l border-gray-400 text-right">
+                {estimateData[0].amount1}
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            {/* <tr>
               <td className="pt-6"></td>
               <td className="pt-6"></td>
               <td className="pt-6">Sub Total</td>
               <td className="pt-6">1000.00</td>
             </tr> */}
-              {/* <tr>
+            {/* <tr>
               <td className="pt-1"></td>
               <td className="pt-1"></td>
               <td className="pt-1">Sales Tax</td>
               <td className="pt-1">100.00</td>
             </tr> */}
-              <tr>
-                <td className="p-2"></td>
-                <td className="p-2"></td>
-                <td className="text-lg pt-6 pb-2 px-2">Total</td>
-                <td className="pt-6 pb-2 px-2 text-right">
-                  {estimateData[0].total}
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-        {/* <div className="flex justify-evenly">
-          <button
-            className="border bg-indigo-700 rounded py-2 px-4 mt-5 text-gray-300 mb-4"
-            onClick={downloadPDF}
-          >
-            Dowload PDF
-          </button> */}
+            <tr>
+              <td className="p-2"></td>
+              <td className="p-2"></td>
+              <td className="text-lg pt-6 pb-2 px-2">Total</td>
+              <td className="pt-6 pb-2 px-2 text-right">
+                {estimateData[0].total}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div className="flex justify-evenly">
+        <button
+          className="border bg-indigo-700 rounded py-2 px-4 mt-5 text-gray-300 mb-4"
+          onClick={downloadPDF}
+        >
+          Dowload PDF
+        </button>
         <div className="flex justify-end">
           <button
             onClick={onClose}
@@ -156,8 +157,7 @@ function EstimateModal({ isOpen, onClose }) {
           </button>
         </div>
       </div>
-      {/* </div> */}
-    </>
+    </div>
   );
 }
 
